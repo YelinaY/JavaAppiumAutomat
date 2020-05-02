bc717f71b7d75499beda155fe7dd6377f7a8746e
YelinaY <eelina1205@gmail.com>
2020-05-02 13:45:33 +0200

    Добавлены тесты ожидания элементов

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,8 +3,12 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.net.URL;
 
 public class FirstTest {
@@ -32,7 +36,22 @@ public class FirstTest {
   public  void firstTest(){
     WebElement element = driver.findElementByXPath("//*[contains(@text,'ADD OR EDIT LANGUAGES')]");
     element.click();
-    
+    WebElement elementk = waitForElementPresentByXpath("//*[contains(@text,'ADD LANGUAGE')]", "Cannot find element", 5);
+        elementk.click();
+
+    //android.widget.TextView[@content-desc="Search for a language"]
+
+    //WebElement element_to_enter_search_line = driver.findElementByXPath("//*[contains(@text,'Search for a language')]");
+    //element_to_enter_search_line.sendKeys("English");
+
    //System.out.println("First test run");
     }
+
+  private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds) {
+    WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+    wait.withMessage(error_message + "\n");
+    By by = By.xpath(xpath);
+    return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+  }
+
 }
