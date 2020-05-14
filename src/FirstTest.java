072b13f7288e7a45dd73fcd16ad3d0dce3cc130f
YelinaY <eelina1205@gmail.com>
2020-05-14 21:07:06 +0200

    IV Рефакторинг. Создаю родительский класс CoreTestCaes

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,46 +1,20 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCaes;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 
 
 
 
-public class FirstTest {
-  private AppiumDriver driver;
-
-  @Before
-
-  public void setUp() throws Exception {
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability("platformName", "Android");
-    capabilities.setCapability("deviceName", "and80");
-    capabilities.setCapability("platformVersion", "8.0");
-    capabilities.setCapability("automationName", "Appium");
-    capabilities.setCapability("appPackage", "org.wikipedia");
-    capabilities.setCapability("app", "C:\\JavaAppiumAutomat\\JavaAppiumAutomat\\apks\\org.wikipedia_50309_apps.evozi.com.apk");
-    capabilities.setCapability("appActivity", ".main.MainActivity");
-    driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-
-  }
-
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
+public class FirstTest extends CoreTestCaes {
 
   @Test
   public void testSwipeToLeftAndRite(){
---
+++ src/lib/CoreTestCaes.java
@@ -0,0 +1,36 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class CoreTestCaes extends TestCase {
+  protected AppiumDriver driver;
+private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+
+@Override
+
+   public void setUp() throws Exception {
+     super.setUp();
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("platformName", "Android");
+    capabilities.setCapability("deviceName", "and80");
+    capabilities.setCapability("platformVersion", "8.0");
+    capabilities.setCapability("automationName", "Appium");
+    capabilities.setCapability("appPackage", "org.wikipedia");
+    capabilities.setCapability("app", "C:\\JavaAppiumAutomat\\JavaAppiumAutomat\\apks\\org.wikipedia_50309_apps.evozi.com.apk");
+    capabilities.setCapability("appActivity", ".main.MainActivity");
+    driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+
+  }
+@Override
+  public void tearDown() throws Exception {
+    driver.quit();
+    super.tearDown();
+  }
+
+}
